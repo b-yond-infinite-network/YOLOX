d26a6ca5600528e1eaad05781e6789cee56d4c81
Aditya-Bobade <aditya.bobade@b-yond.com>
2022-05-19 21:38:01 +0530

    validation loss logging

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -201,6 +201,62 @@ class Exp(BaseExp):
 
         return train_loader
 
+    def get_val_loader(
+        self, batch_size, is_distributed, no_aug=False, cache_img=False, testdev=False
+    ):
+        from yolox.data import (
+            COCODataset,
+            TrainTransform,
+            YoloBatchSampler,
+            DataLoader,
+            InfiniteSampler,
+            MosaicDetection,
+            worker_init_reset_seed,
+        )
+        from yolox.utils import (
+            wait_for_the_master,
+            get_local_rank,
+        )
+
+        local_rank = get_local_rank()
+
+        with wait_for_the_master(local_rank):
+            dataset = COCODataset(
+                data_dir=self.data_dir,
+                json_file=self.val_ann if not testdev else self.test_ann,
+                img_size=self.input_size,
+                preproc=TrainTransform(
+                    max_labels=50,
+                    flip_prob=0.0,
+                    hsv_prob=0.0),
+                cache=cache_img,
+            )
+
+        self.dataset = dataset
+
+        if is_distributed:
+            batch_size = batch_size // dist.get_world_size()
+
+        sampler = InfiniteSampler(len(self.dataset), seed=self.seed if self.seed else 0)
+
+        batch_sampler = YoloBatchSampler(
+            sampler=sampler,
+            batch_size=batch_size,
+            drop_last=False,
+            mosaic=not no_aug,
+        )
+
+        dataloader_kwargs = {"num_workers": self.data_num_workers, "pin_memory": True}
+        dataloader_kwargs["batch_sampler"] = batch_sampler
+
+        # Make sure each process has different random seed, especially for 'fork' method.
+        # Check https://github.com/pytorch/pytorch/issues/63311 for more details.
+        dataloader_kwargs["worker_init_fn"] = worker_init_reset_seed
+
+        val_loader = DataLoader(self.dataset, **dataloader_kwargs)
+
+        return val_loader
+
     def random_resize(self, data_loader, epoch, rank, is_distributed):
         tensor = torch.LongTensor(2).cuda()
 
